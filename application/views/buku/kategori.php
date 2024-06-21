163ca70aa74ae77c00ac3fe5973e585ab199ebed
rohidtzz <63803545+rohidtzz@users.noreply.github.com>
2024-06-21 15:40:31 +0700

    fixing kategori

--- application/views/buku/kategori.php
+++ application/views/buku/kategori.php
@@ -51,14 +51,7 @@
             <form action="<?= base_url('buku/kategori'); ?>" method="post">
                 <div class="modal-body">
                     <div class="form-group">
-                        <select name="kategori" class="form-control form-control-user">
-                            <option value="">Pilih Kategori</option>
-                            <?php
-                            $k = ['Sains','Hobby','Komputer','Komunikasi','Hukum','Agama','Populer','Bahasa','Komik'];
-                            for ($i=0;$i<9;$i++) { ?>
-                            <option value="<?= $k[$i];?>"><?= $k[$i];?></option>
-                            <?php } ?>
-                        </select>
+                        <input type="text" class="form-control form-control-user" id="kategori" name="kategori" placeholder="Masukkan Kategori" >
                     </div>
                 </div>
                 <div class="modal-footer">
